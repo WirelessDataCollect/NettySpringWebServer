6fec6a05df0a03ff7f894f4cde8b15f17bd5e7fa
Neyzoter <sonechaochao@gmail.com>
2018-10-21 20:52:13 +0800

    enrich login.jsp

---
+++ src/main/java/com/nesc/NettySpringWebServer/LoginController.java
@@ -0,0 +1,29 @@
+package com.nesc.NettySpringWebServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+@Controller
+//@RequestMapping("/ServerWeb")  //控制器WebController中提到的虚拟子文件夹
+public class LoginController{ 
+	//GET:表单中设定的参数和参数值将附加到页面地址的末尾以参数的形式提交
+   @RequestMapping(value = "/login",method = RequestMethod.GET)//地址
+   public ModelAndView admin() {
+	   return new ModelAndView("login","command",new Admin());//jsp文件
+   }
+   //POST：在提交表单时，表达中的参数将作为请求头中的信息发送
+   @RequestMapping(value = "/working",method = RequestMethod.POST)  //这里的value指的是地址
+   public String login(@ModelAttribute("SpringWeb")Admin admin,ModelMap model) {
+	   if(admin.getName().equals("song") && admin.getKey().equals("123456")) {
+		      model.addAttribute("name", admin.getName());
+		      model.addAttribute("key", admin.getKey());
+		      return "working";  //result视图从service方法中返回，呈现result.jsp  
+	   }
+	   else {
+		   return "verifailed";
+	   }
+   }
+}
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,12 +1,17 @@
-<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>  <%-- 自定义标签--%>
+<%@page language="java" import="java.util.*" pageEncoding="UTF-8" %> <!-- 设置页面的属性 -->
 <html>
 <head>
     <title>Spring MVC Form Handling</title>
+    <%
+    String IP = request.getRemoteAddr();
+    String Info = request.getHeader("user-agent");
+    %>
 </head>
 <body>
 
 <h2>Login</h2>
-<form:form method="POST" action="/NettySpringWebServer/login">
+<form:form method="GET" action="/NettySpringWebServer/working">
    <table>
     <tr>
         <td><form:label path="name">Name</form:label></td>
@@ -19,6 +24,18 @@
     <tr>
         <td colspan="2">
             <input type="submit" value="Submit"/>
+        </td>
+    </tr>
+
+     <tr>
+        <td colspan="2">   
+		<p>您计算机的IP: <%=IP %> <br>
+		您计算机的类型：<%=Info %></p>
+        </td>
+    </tr>
+     <tr>
+        <td colspan="2">   
+		<%=new Date()%> <%-- 显示当前时间 --%>
         </td>
     </tr>
 </table>  
