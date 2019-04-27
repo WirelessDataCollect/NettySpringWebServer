6329683fd314548ae23757e3614ec4485c9a6104
Neyzoter <sonechaochao@gmail.com>
2019-04-27 20:22:19 +0800

    modified TCP_ServerHandler.java's log

--- src/main/java/com/sorl/backend/TCP_ServerHandler.java
+++ src/main/java/com/sorl/backend/TCP_ServerHandler.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 
+import org.apache.log4j.Logger;
+
 /**
 * 
 * TCP服务器的输入处理器函数
@@ -14,6 +16,7 @@ import io.netty.util.ReferenceCountUtil;
 * @version 0.0.1
 */
 public class TCP_ServerHandler extends ChannelInboundHandlerAdapter {
+	private static final Logger logger = Logger.getLogger(TCP_ServerHandler.class);
 	private DataProcessor processor;
 	TCP_ServerHandler(){
 		processor =(DataProcessor) App.getApplicationContext().getBean("dataProcessor");//获取一个数据处理器
@@ -34,13 +37,11 @@ public class TCP_ServerHandler extends ChannelInboundHandlerAdapter {
     }
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {   //1
-        System.out.println(
-                "Device TCP channel " + ctx.channel().toString() + " created");
+		logger.info("Device TCP channel " + ctx.channel().toString() + " created");
     }
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        // 当出现异常就关闭连接
-        cause.printStackTrace();
+    	logger.error("",cause);
         ctx.close();
     }
 }
